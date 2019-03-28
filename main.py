4a80cc29d8fea7c318e99bf56fb5a6ea0baa5f72
esterfogaca <ester@cantarim.com>
2019-03-27 23:16:44 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from operator import itemgetter
+
+class SellersRancking:
+    def best_seller(self, sellers):
+        return []
+
+    def rancking_list(self, sellers):
+        return []
+
+    def best_seller_store(self, sellers, store):
+        return []
+
+    def sales_goals(self, sellers):
+        return []
